c7e16b66f298ec7d3fc1d17378524610a9009177
balusoman <balucritaxzz@gmail.com>
2025-01-14 10:54:57 +0530

    lumpsum added

---
+++ src/app/sip-lumpsum/sip-lumpsum.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sip-lumpsum',
+  imports: [],
+  templateUrl: './sip-lumpsum.component.html',
+  styleUrl: './sip-lumpsum.component.scss'
+})
+export class SipLumpsumComponent {
+
+}
